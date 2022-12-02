abdaedda2dbdbdb0e7c5429c0126a6fea6b2b222
KemapaX <kpalmieri@utb.edu.co>
2022-12-01 20:37:39 -0500

    Aplicando CORS

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 import controller as apis
 
 server = Flask("Api-tables")
+CORS(server)
 
-#this 
+
+@cross_origin
 @server.get('/')
 def api():
     return jsonify({'messege': "Welcome to Api-Tables"})
 
+
+@cross_origin
 @server.get('/show')
 def show_table():
     return apis.show() 
