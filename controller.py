f8acc245f0e45c1493ce9e035f41c8aa8df30045
KemapaX <kpalmieri@utb.edu.co>
2022-12-03 20:17:01 -0500

    En esta actulizacion se agregaron mas resultados de la tabla de equipo del mundial-2022, para la proxima actulizacion, hacer el codigo mas limpio

--- controller.py
+++ controller.py
@@ -4,9 +4,9 @@ from model.table import ScrapingTableInformation
 
 def show():
     # this is the url of the page we want to scrape
-    url = 'https://colombia.as.com/resultados/futbol/mundial/2022/clasificacion/'
+    url_as = 'https://colombia.as.com/resultados/futbol/mundial/2022/clasificacion/'
 
     # tell the object to get the url
-    data = ScrapingTableInformation().pages(url)
+    data = ScrapingTableInformation().pages(url_as)
 
     return jsonify({"records": data, 'result': len(data)})
--- model/table.py
+++ model/table.py
@@ -3,31 +3,38 @@ from bs4 import BeautifulSoup
 
 class ScrapingTableInformation:
     def __init__(self):
-        self.list_teams_name = list()
-        self.list_teams_logo = list()
-        self.list_teams_pts = list()
-
         self.tabla = []
         
     
-    def pages(self, url):
+    def pages(self, *args):
         # this is the html from the given url
-        page = requests.request("GET", url)
-
+        page_as = requests.request("GET", args[0])
+        
         # this do the convert to Beautiful format. it allow identify the different element from HTML    
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup_as = BeautifulSoup(page_as.content, 'html.parser')
 
         # find_all('element', attribute_=' ') -> find everything, using the indicated parameters
-        teams_name = soup.find_all('span', class_='nombre-equipo')
-        teams_pts = soup.find_all('td', class_='destacado')
+        teams_name = soup_as.find_all('span', class_='nombre-equipo')
+        teams_pts = soup_as.find_all('td', class_='destacado')
+        teams_pj = soup_as.find_all('td', class_='')
+        teams_logo = soup_as.find_all('th', class_='cont-nombre-equipo')
+        
+        """ for the table of teams lack | GF GC DG | 
+        
+        For the next update, improve the logic of all the functions
+        
+        """
+        
 
         # add information to list
         data_name=self.add_information(teams_name)
         data_pts=self.add_information(teams_pts)
-
+        data_pj=self.add_information(teams_pj)
+        data_logo=self.add_information_img(teams_logo, 'image')
+        
         # add information to table
-        tabla=self.add_table(data_name, data_pts)
-
+        tabla=self.add_table(data_name, data_pts, data_pj, data_logo)
+        
         return tabla
 
 
@@ -39,14 +46,29 @@ class ScrapingTableInformation:
                 list_teams.append(teams_x.text)
         return list_teams
     
-    # this need two parameters for it can add information to table of teams and resulted
-    def add_table(self, data_a, data_b):
+    
+    # this function give img of the teams
+    def add_information_img(self, data, attribute):
         list_teams = list()
-        n = 0
-        for i in data_a:
+        for teams_x in data:
+            logo = teams_x.find('img', itemprop=attribute).get('data-src')
+            
+            list_teams.append(logo)
+        return list_teams
+    
+    
+    # this need two parameters for it can add information to table of teams and resulted
+    def add_table(self, *args):
+        list_teams = list()
+        n = 0; p = 0
+        
+        for i in args[0]:
             n+=1
             list_teams.append(
-                {'id': n, 'name': i, 'pts': data_b[data_a.index(i)]})
+                {'id': n, 'name': i, 'PTJ': args[1][args[0].index(i)], 'PJ': args[2][p],
+                'PG': args[2][p+1], 'PE': args[2][p+2], 'PP': args[2][p+3], 'img-logo': args[3][n-1]})
+            p+=4
+        
         return list_teams
 
 
